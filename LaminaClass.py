57bc72661fe5391e22fd0341dfb583acb32c8615
David Canosa Ybarra <d.canosaybarra@student.tudelft.nl>
2024-03-22 11:45:06 +0100

    Not Much :)

--- LaminaClass.py
+++ LaminaClass.py
@@ -31,6 +31,8 @@ class Lamina():
 		self.QMatrix[1, 0] = self.QMatrix[0, 1] = self.v12 * self.E2 / self.Q
 		self.QMatrix[2, 2] = self.G12
 
+	def calcStresses(self, globalStrains):
+		pass
 
 if __name__ == '__main__':
 	# Testing function to match lecture values
