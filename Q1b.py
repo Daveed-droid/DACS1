f76ef75227506b439978f3f65094e1df579ded9d
Daveed-droid <d.canosaybarra@gmail.com>
2024-03-27 18:36:33 +0100

    added global stresses and strains

--- Q1b.py
+++ Q1b.py
@@ -7,15 +7,20 @@
 from AssignmentData import Lamina_mean
 from LaminateClass import Laminate
 import numpy as np
-
+from matplotlib import pyplot as plt
 
 def Q1b(Lamina_mean = Lamina_mean, Laminate = Laminate):
 	LayUp = [0, 0, 90, 30, 90]
 	Laminate = Laminate(LayUp, Lamina_mean)
 	Load = np.array([0.2e2, 1.8e4, 0, 18e3, 0, 0]).T
-	print(Laminate.calcPlyStrains(Load))
-	print(Laminate.calcStresses(Load))
-	plt.plot(Laminate)
+	strains = Laminate.calcGloStrains(Load)
+	stresses = Laminate.calcGloStresses(Load)
+	print(strains)
+	print(stresses)
+	z = Laminate.zlst
+	data = [[Laminate.zlst[0],stresses[0,1]]]+[[z, st] for z, st in zip(Laminate.zlst, stresses[0,:])]+[[Laminate.zlst[-1],stresses[0,-1]]]
+	plt.plot(data)
+	plt.show()
 
 
 Q1b()
