a29ae9b3d210e1d708366a6995d9a6a231315000
gabr <125443609+JHJOECW@users.noreply.github.com>
2024-04-02 00:06:04 +0200

    Q2

--- Q2a.py
+++ Q2a.py
@@ -21,41 +21,84 @@ def Q2a(Lamina_mean = Lamina_mean, Laminate = Laminate):
     #Laminate.calcABD()
     angle = np.arange(0,181,20)
     sig = np.array([Xt_mean,Yt_mean,Xc_mean,Yc_mean,S_mean])
+    Nf = np.zeros([2,len(angle)])
+    Nl = np.zeros([2, len(angle)])
+    for i in range(0,len(angle)):
 
-
-    for i in angle:
-
-        i = 0
-        dn = 50 # Load increment
+        failure = 0
+        dn = 500000 # Load increment
         lpf = False
+        fpf = False
 
+        #Ns = dn * np.sin(np.deg2rad(angle[i]))
+
+        #Ny = dn * np.cos(np.deg2rad(angle[i]))
+        #Load = np.array([0, Ny, Ns, 0, 0, 0]).T
+
+        plys = Laminate(LayUp, Lamina_mean)
+        #stresses = plys.calcPlyStresses(Load)
+        print(plys.ABD)
         while lpf == False:
 
-            dn = dn*(i+1)
+
             Ns = dn*np.sin(np.deg2rad(angle[i]))
 
             Ny = dn*np.cos(np.deg2rad(angle[i]))
             Load = np.array([0, Ny, Ns, 0, 0, 0]).T
-            plys = Laminate(LayUp, Lamina_mean)
+
             stresses = plys.calcPlyStresses(Load)
-            print(stresses)
+            #print(stresses)
+            print(angle[i])
+            print("test")
             #Apply stress failure criteria
-            for j in LayUp:
+            for j in range(0,len(LayUp)):
 
             # Puck Fibre Failure
-                if stresses[i,0] > 0:
-                    f_xff = stresses[i,0] / Xt_mean
+                if stresses[0,j] > 0:
+                    f_xff = stresses[0,j] / Xt_mean
                 else:
-                    f_xff = stresses[i, 0] / -Xc_mean
-                if stresses[i,1] > 0:
-                    f_yff = stresses[i,1] / Yt_mean
+                    f_xff = stresses[0,j] / -Xc_mean
+                if stresses[1,j] > 0:
+                    f_yff = stresses[1,j] / Yt_mean
                 else:
-                    f_yff = stresses[i, 1] / -Yc_mean
-                if stresses[i,2] > 0:
-                    f_sff = stresses[i,2] / S_mean
+                    f_yff = stresses[1,j] / -Yc_mean
+                if stresses[2,j] > 0:
+                    f_sff = stresses[2,j] / S_mean
                 else:
-                    f_sff = stresses[i, 2] / -S_mean
-                if f_xff,f_yff
+                    f_sff = stresses[2,j] / -S_mean
+
+
+
+                if angle[i] <90:   #Mode A
+                    pass
+
+                elif angle[i] >= 90:# and stresses[i,1] >= -?? Mode B
+                    pass
+
+                else:#Mode C
+                    pass
+
+                # Determine which failure mode, and apply deg rule
+                if f_xff >= 1 or f_yff >= 1:
+                    if fpf == False:
+                        Nf[0,i] = Ny
+                        Nf[1,i] = Ns
+                        fpf = True
+                    elif failure >= 3:
+                        Nl[0, i] = Ny
+                        Nl[1, i] = Ns
+                        lpf = True
+                    if LayUp[j] == 45 or LayUp[j] == -45:
+                        plys.ABD[0:3,0:3] = plys.ABD[0:3,0:3] - 8*plys.QGlobalAr[j]*t
+                    else:
+                        plys.ABD[0:3, 0:3] = plys.ABD[0:3, 0:3] - 8 * plys.QGlobalAr[j]*t
+                    failure = failure +1
+
+                    break
+
+            dn = dn * (i + 1)
+
+    return [Nf, Nl]
 
 
 
@@ -63,11 +106,11 @@ def Q2a(Lamina_mean = Lamina_mean, Laminate = Laminate):
 
 
 
-            i = i+1
-            print(i)
-            lpf = True
+
 
 
 
 Q2a()
+
+print(Q2a()[0])
 
